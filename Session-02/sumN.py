4c2e68b4ae8979cb1c1e8a45233e43b1f7ec765a
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-02-04 15:49:05 +0100

    Session 2

--- Session-02/sumN.py
+++ Session-02/sumN.py
@@ -1,3 +1,5 @@
+#Function for calculating the sum of the N first integer numbers
+
 def sumn(n):
     res = 0
     for i in range(1, n+1):
@@ -7,4 +9,4 @@ def sumn(n):
 
 # -- The main program starts here
 print("Sum of the 20 first integers: ", sumn(20))
-print("Sum of the 100, frist integers: ", sumn(100))
+print("Sum of the 100, first integers: ", sumn(100))
