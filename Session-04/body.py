7d0366823c2a19c8c2d7380fc269d27403243039
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-02-11 16:18:10 +0100

    Session-04. Ex 2

--- Session-04/body.py
+++ Session-04/body.py
@@ -3,9 +3,9 @@ from pathlib import Path
 FILENAME = "RNU6_269P.txt"
 
 file_contents = Path(FILENAME).read_text()
-header = file_contents.split('\n')
-header = join
+file = file_contents.split('\n')
+body = "".join(file[1:])
 
 print("Body of the U5.txt file:")
-print(header[1:])
+print(body)
 
