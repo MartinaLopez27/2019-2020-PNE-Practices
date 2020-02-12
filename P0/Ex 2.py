9f6a1b2dfc5a2716ca0196f02b0c11a4a23522d8
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-02-12 17:46:33 +0100

    Session-05. Ex 2

---
+++ P0/Ex 2.py
@@ -0,0 +1,8 @@
+from Seq0 import *
+
+FOLDER = "../Session-04/"
+filename = "U5.txt"
+
+print("DNA file:", filename)
+seq = seq_read_fasta(FOLDER + filename)
+print("The first 20 bases are: ", seq[0:20])
