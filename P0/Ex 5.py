c2f81cd1d8c8131a252f402d68457ece75606f8b
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-02-18 22:15:42 +0100

    Session06-Ex 5

--- P0/Ex 5.py
+++ P0/Ex 5.py
@@ -1,11 +1,12 @@
 from Seq0 import *
 
 FOLDER = "../Session-04/"
-filename = ["U5.txt", "ADA.txt", "FRAT1.txt", "FXN.txt", "RNU6_269P.txt"]
+ext = ".txt"
+filename = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
 base = ["A", "C", "T", "G"]
 
 print("-----| Exercise 5 |------")
 
 for element in filename:
-    seq = seq_read_fasta(FOLDER + element)
+    seq = seq_read_fasta(FOLDER + element + ext)
     print(f"Gene {element}: {seq_count(seq)}")
