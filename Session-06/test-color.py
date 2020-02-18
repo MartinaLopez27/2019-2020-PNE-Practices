3a903be8b96f8624d8f531f5f69ff3d3097e1022
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-02-18 16:07:27 +0100

    Session06-test color

--- Session-06/test-color.py
+++ Session-06/test-color.py
@@ -1,1 +1,6 @@
 import termcolor
+
+termcolor.cprint("testing the colors", "green")
+termcolor.cprint("testing the colors", "blue")
+termcolor.cprint("testing the colors", "red")
+termcolor.cprint("testing the colors", "cyan")
