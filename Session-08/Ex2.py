15810e5cc6e5145940e156f9c0273d4519a58ba2
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-02-25 15:56:40 +0100

    Session-08. Ex 2

---
+++ Session-08/Ex2.py
@@ -0,0 +1,12 @@
+Exercise = 2
+print(f"------| Exercise {Exercise} |------")
+
+Destination IP	     Name (if known)	 Time
+212.128.253.145      Self	             0.020 ms, 0.017 ms...
+212.128.255.129	     f-l3208-pc01
+8.8.8.8	             Google              3.67 ms, 3.76 ms
+10.3.34.133          Mobile              2086 ms, 1057 ms
+212.128.240.50       www.urjc.es         2.27 ms, 2.40 ms
+140.82.118.4         github.com          36.8 ms, 36.2 ms
+208.80.154.224       wikipedia.org       133 ms, 134 ms
+103.223.184.84       newdomain.com.au    308 ms, 308 ms
