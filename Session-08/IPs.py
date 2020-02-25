11748c8049be1921c0b345c5e12f3ac92b19d8f3
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-02-25 15:37:38 +0100

    Session-08. Ex 1

---
+++ Session-08/IPs.py
@@ -0,0 +1,7 @@
+Exercise = 1
+print(f"------| Exercise {Exercise} |------")
+
+Machine	       Interface	Date	      Place	   IP
+Mobile  	   Wifi         25/02/2020    Lab      10.3.34.133
+Lab computer   Wire         25/02/2020    Lab      212.128.253.145
+
