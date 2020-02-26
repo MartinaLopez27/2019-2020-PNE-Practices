29fdef3c55438a7968b0dc387acecc5fc3cf4ef4
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-02-26 19:58:19 +0100

    Session09-Ex 5

---
+++ P2/Ex 5.py
@@ -0,0 +1,26 @@
+from Client0 import Client
+from Seq1 import Seq
+
+PRACTICE = 2
+EXERCISE = 5
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+folder = "../Session-04/"
+ext = ".txt"
+gene = "U5"
+
+IP = "212.128.253.145"
+PORT = 8080
+
+# -- Create a client object
+c = Client(IP, PORT)
+
+# -- Print it
+print(c)
+
+
+file = Seq().read_fasta(folder + gene + ext)
+
+c.debug_talk(f"Sending {gene} Gene to the server...")
+c.debug_talk(str(file))
