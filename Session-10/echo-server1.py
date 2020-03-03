120a6553c7ce97d44f453ae379aae0bc412922c2
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-03-03 19:19:27 +0100

    Session10- echo-server1

--- Session-10/echo-server1.py
+++ Session-10/echo-server1.py
@@ -24,3 +24,46 @@ ls.bind((IP, PORT))
 ls.listen()
 
 print("The server is configured!")
+
+while True:
+    # -- Waits for a client to connect
+    print("Waiting for Clients to connect")
+
+    try:
+        (cs, client_ip_port) = ls.accept()
+
+    # -- Server stopped manually
+    except KeyboardInterrupt:
+        print("Server stopped by the user")
+
+        # -- Close the listenning socket
+        ls.close()
+
+        # -- Exit!
+        exit()
+
+    # -- Execute this part if there are no errors
+    else:
+
+        print("A client has connected to the server!")
+
+        # -- Read the message from the client
+        # -- The received message is in raw bytes
+        msg_raw = cs.recv(2048)
+
+        # -- We decode it for converting it
+        # -- into a human-redeable string
+        msg = msg_raw.decode()
+
+        # -- Print the received message
+        print("Message received: ", end="")
+        termcolor.cprint(msg, "green")
+
+        # -- Send a response message to the client
+        response = "ECHO: " + msg + "\n"
+
+        # -- The message has to be encoded into bytes
+        cs.send(response.encode())
+
+        # -- Close the data socket
+        cs.close()
