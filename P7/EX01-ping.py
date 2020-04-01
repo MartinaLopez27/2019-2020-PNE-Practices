0e34382aea725da67a60df53bdabfba41b99faba
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-04-01 18:41:18 +0200

    Session19 - EX01-ping.py

--- P7/EX01-ping.py
+++ P7/EX01-ping.py
@@ -20,17 +20,17 @@ except ConnectionRefusedError:
     print("ERROR! Cannot connect to the Server")
     exit()
 
-# -- Read the response message from the server
+# Read the response message from the server
 r1 = conn.getresponse()
 
-# -- Print the status line
+# Print the status line
 print(f"Response received!: {r1.status} {r1.reason}\n")
 
-# -- Read the response's body
+# Read the response's body
 data1 = r1.read().decode()
 
-# -- Create a variable with the data,
-# -- form the JSON received
+# Create a variable with the data,
+# form the JSON received
 response = json.loads(data1)
 
 ping = response['ping']
