2a09789412a868708003f98a9fbf6af15bdaf281
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-04-20 16:24:12 +0200

    Session20 - index.html

--- Final-Project/server.py
+++ Final-Project/server.py
@@ -1,203 +1,230 @@
 import http.server
 import socketserver
 import termcolor
-from pathlib import Path
-from Seq1 import Seq
+import json
+import http.client
+from Seq import Seq
 
 # Define the Server's port
-PORT = 8080
-
-
-# -- This is for preventing the error: "Port already in use"
-socketserver.TCPServer.allow_reuse_address = True
-
-# -- Sequences for the GET command
-SEQ_GET = [
-    "ACCTCCTCTCCAGCAATGCCAACCCCAGTCCAGGCCCCCATCCGCCCAGGATCTCGATCA",
-    "AAAAACATTAATCTGTGGCCTTTCTTTGCCATTTCCAACTCTGCCACCTCCATCGAACGA",
-    "CAAGGTCCCCTTCTTCCTTTCCATTCCCGTCAGCTTCATTTCCCTAATCTCCGTACAAAT",
-    "CCCTAGCCTGACTCCCTTTCCTTTCCATCCTCACCAGACGCCCGCATGCCGGACCTCAAA",
-    "AGCGCAAACGCTAAAAACCGGTTGAGTTGACGCACGGAGAGAAGGGGTGTGTGGGTGGGT",
-]
-
-FOLDER = "../Session-04/"
-EXT = ".txt"
+PORT = 8000
 
 
 # Class with our Handler. It is a called derived from BaseHTTPRequestHandler
 # It means that our class inheritates all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
+    def dictionary_split(self, path):
+        dictionary = dict()
+        if '?' in self.path:
+            dicc = self.path.split("?")[1]
+            dicc = dicc.split(" ")[0]
+            piece = dicc.split("&")
+            for pair in piece:
+                if '=' in pair:
+                    key = pair.split("=")[0]
+                    value = pair.split("=")[1]
+                    dictionary[key] = value
+        return (dictionary)
+
 
     def do_GET(self):
+        response_code = 200
+        json_response = False
+
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
 
         # Print the request line
         termcolor.cprint(self.requestline, 'green')
 
-        # Analize the request line
-        req_line = self.requestline.split(' ')
+        # IN this simple server version:
+        # We are NOT processing the client's request
+        # It is a happy server: It always returns a message saying
+        # that everything is ok
 
-        # Get the path. It always start with the / symbol
-        path = req_line[1]
+        # -- Message to send back to the client
+        if self.path == "/":
+            with open("index.html", "r") as f:
+                contents = f.read()
+        # -- Here we read the index.html code.
 
-        # Read the arguments
-        arguments = path.split('?')
 
-        # Get the verb. It is the first argument
-        verb = arguments[0]
-
-        # -- Content type header
-        # -- Both, the error and the main page are in HTML
-        contents = Path('Error.html').read_text()
-        error_code = 404
-
-        if verb == "/":
-            # Open the form1.html file
-            # Read the index from the file
-            contents = Path('form-4.html').read_text()
-            error_code = 200
-        elif verb == "/listSpecies":
-            contents = """
-            <!DOCTYPE html>
-            <html lang = "en">
-            <head>
-            <meta charset = "utf-8" >
-              <title> LIST SPECIES </title >
-            </head >
-            <body>
-            <p> The total number of species in the ensembl is: </p>
-            <p> The limit you have selected is: </p>
-            <p> The names of the species are: </p>
-            <a href="/">Main page</a>
-            </body>
-            </html>
-            """
-            error_code = 200
-        elif verb == "/get":
-            # -- Get the argument to the right of the ? symbol
-            pair = arguments[1]
-            # -- Get all the pairs name = value
-            pairs = pair.split('&')
-            # -- Get the two elements: name and value
-            name, value = pairs[0].split("=")
-            n = int(value)
-
-            # -- Get the sequence
-            seq = SEQ_GET[n]
-
-            # -- Generate the html code
-            contents = f"""
-                        <!DOCTYPE html>
-                        <html lang = "en">
-                        <head>
-                        <meta charset = "utf-8" >
-                          <title> GET </title >
-                        </head >
-                        <body>
-                        <h2> Sequence number {n}</h2>
-                        <p> {seq} </p>
-                        <a href="/">Main page</a>
-                        </body>
-                        </html>
-                        """
-            error_code = 200
-        elif verb == "/gene":
-            # -- Get the argument to the right of the ? symbol
-            pair = arguments[1]
-            # -- Get all the pairs name = value
-            pairs = pair.split('&')
-            # -- Get the two elements: name and value
-            name, gene = pairs[0].split("=")
-
-            s = Seq()
-            s.read_fasta(FOLDER + gene + EXT)
-            gene_str = str(s)
-            # -- Generate the html code
-            contents = f"""
-                        <!DOCTYPE html>
-                        <html lang = "en">
-                        <head>
-                        <meta charset = "utf-8" >
-                          <title> GENE </title >
-                        </head >
-                        <body>
-                        <h2> Gene: {gene}</h2>
-                        <textarea readonly rows="20" cols="80"> {gene_str} </textarea>
-                        <br>
-                        <br>
-                        <a href="/">Main page</a>
-                        </body>
-                        </html>
-                        """
-            error_code = 200
-        elif verb == "/operation":
-            # -- Get the argument to the right of the ? symbol
-            pair = arguments[1]
-            # -- Get all the pairs name = value
-            pairs = pair.split('&')
-            # -- Get the two elements: name and value
-            name, seq = pairs[0].split("=")
-            # -- Get the two elements of the operation
-            name, op = pairs[1].split("=")
-
-            # -- Create the sequence
-            s = Seq(seq)
-
-            if op == "comp":
-                result = s.complement()
-            elif op == "rev":
-                result = s.reverse()
+        elif "listSpecies" in self.path:
+            dicc = self.dictionary_split(self.path)
+            if 'limit' in dicc:
+                try:
+                    limit = int(dicc['limit'])
+                except:
+                    limit = 0
             else:
-                sl = s.len()
-                ca = s.count_base('A')
-                pa = "{:.1f}".format(100 * ca / sl)
-                cc = s.count_base('C')
-                pc = "{:.1f}".format(100 * cc / sl)
-                cg = s.count_base('G')
-                pg = "{:.1f}".format(100 * cg / sl)
-                ct = s.count_base('T')
-                pt = "{:.1f}".format(100 * ct / sl)
+                limit = 0
 
-                result = f"""
-                <p>Total length: {sl}</p>
-                <p>A: {ca} ({pa}%)</p>
-                <p>C: {cc} ({pc}%)</p>
-                <p>G: {cg} ({pg}%)</p>
-                <p>T: {ct} ({pt}%)</p>"""
+            conn = http.client.HTTPConnection('rest.ensembl.org')
+            conn.request("GET", "/info/species?content-type=application/json")
+            result = conn.getresponse()
 
-            contents = f"""
-                        <!DOCTYPE html>
-                        <html lang = "en">
-                        <head>
-                        <meta charset = "utf-8" >
-                          <title> OPERATION </title >
-                        </head >
-                        <body>
-                        <h2> Sequence </h2>
-                        <p>{seq}</p>
-                        <h2> Operation: </h2>
-                        <p>{op}</p>
-                        <h2> Result: </h2>
-                        <p>{result}</p>
-                        <br>
-                        <br>
-                        <a href="/">Main page</a>
-                        </body>
-                        </html>
-                        """
-            error_code = 200
+            # -- Print the status
+            print()
+            print("Response received: ", end='')
+            print(result.status, result.reason)
 
-        # Generating the response message
-        self.send_response(error_code)  # -- Status line: OK!
+            # -- Read the response's body and close
+            # -- the connection
+            text_json = result.read().decode("utf-8")
+            response = json.loads(text_json)
+            print(response)
 
-        # Define the content-type header:
-        self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', len(str.encode(contents)))
+            species = response['species']
+            if 'json' in dicc and dicc['json'] == '1':
+                json_response = True
+                if limit == 0:
+                    limit = len(species)
+                newlist = species[1:limit]
+                contents = json.dumps(newlist)
+            else:
+                if limit == 0:
+                    limit = len(species)
 
-        # The header is finished
-        self.end_headers()
+                print("Limit: ", limit)
+                contents = """
+                                       <html>
+                                       <body style ="background-color: lavender;">
+                                       <ul> """
 
-        # Send the response message
-        self.wfile.write(str.encode(contents))
+                count = 0
+                for specie in species:
+                    contents = contents + "<li>" + specie['display_name'] + "</li>"
+                    count = count + 1
+                    print(count, limit)
+                    if (count == limit):
+                        break
 
-        return
+                contents = contents + """<ul>
+                                           <body>
+                                           <html>
+                                           """
 
+            conn.close()
+
+
+        elif "karyotype" in self.path:
+            dicc = self.dictionary_split(self.path)
+            if 'specie' in dicc and dicc['specie'] != '':
+                specie = dicc['specie']
+                conn = http.client.HTTPConnection('rest.ensembl.org')
+                conn.request("GET", "/info/assembly/" + specie + "?content-type=application/json")
+                result = conn.getresponse()
+                text_json = result.read().decode("utf-8")
+                response = json.loads(text_json)
+                print(response)
+                if 'karyotype' in response:
+                    list_chromosome = response['karyotype']
+                    if 'json' in dicc and dicc['json'] == '1':
+                        json_response = True
+                        print(list_chromosome)
+                        contents = json.dumps(list_chromosome)
+                    else:
+                        contents = """
+                                      <html>
+                                      <body style ="background-color: salmon;">
+                                      <ul> """
+
+                        for chromosome in list_chromosome:
+                            contents = contents + "<li>" + chromosome + "</li>"
+
+                        contents = contents + """</ul>
+                                              </body>
+                                              </html>
+                                              """
+                else:
+                    response_code = 400
+                    f = open('Error.html', 'r')
+                    contents = f.read()
+            else:
+                response_code = 400
+                f = open('Error.html', 'r')
+                contents = f.read()
+
+
+        elif "chromosomeLength" in self.path:
+            dicc = self.dictionary_split(self.path)
+            if 'chromosome' in dicc and 'specie' in dicc and dicc['chromosome'] != '' and dicc['specie'] != '':
+                u_chromo = dicc['chromosome']
+                specie = dicc['specie']
+                print(u_chromo)
+                print(specie)
+                conn = http.client.HTTPConnection('rest.ensembl.org')
+                conn.request("GET", "/info/assembly/" + specie + "?content-type=application/json")
+                result = conn.getresponse()
+                text_json = result.read().decode("utf-8")
+                response = json.loads(text_json)
+                info = response['top_level_region']
+
+                if 'json' in dicc and dicc['json'] == '1':
+                    json_response = True
+                    long = 0
+                    for element in info:
+                        if (element['name'] == u_chromo):
+                            long = element['length']
+                    dic = dict()
+                    dic['len'] = long
+                    contents = json.dumps(dic)
+                else:
+                    long = 0
+                    for element in info:
+                        if (element['name'] == u_chromo):
+                            long = element['length']
+                    contents = """
+                                      <html>
+                                      <body style ="background-color: salmon;">
+                                      <ul> """
+
+                    contents = contents + "<li>" + str(long) + "</li>"
+
+                    contents = contents + """</ul>
+                                               </body>
+                                               </html>"""
+            else:
+                response_code = 400
+                f = open('Error.html', 'r')
+                contents = f.read()
+
+            # -- Generating the response message
+            self.send_response(response_code)  # -- Status line: OK!
+            if (json_response == True):
+                # Define the content-type header:
+                self.send_header('Content-Type', 'application/json')
+            else:
+                self.send_header('Content-Type', 'text/html')
+
+            self.send_header('Content-Length', len(str.encode(contents)))
+
+            # The header is finished
+            self.end_headers()
+
+            # Send the response message
+            self.wfile.write(str.encode(contents))
+
+            return
+
+    # ------------------------
+    # - Server MAIN program
+    # ------------------------
+    # -- Set the new handler
+    Handler = TestHandler
+    socketserver.TCPServer.allow_reuse_address = True
+
+    # -- Open the socket server
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
+
+    print("")
+    print("Server Stopped")
