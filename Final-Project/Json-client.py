09922d88a4bb55c1a2dfa1855f7e05b86665d668
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-04-24 12:54:03 +0200

    Session20 - json_client.py

--- Final-Project/Json-client.py
+++ Final-Project/Json-client.py
@@ -6,61 +6,61 @@ SERVER = 'localhost'
 
 print("\nConnecting to server: {}:{}\n".format(SERVER, PORT))
 
-# Connect with the server
 conn = http.client.HTTPConnection(SERVER, PORT)
 
-# -- 1 TRY FOR JSON
+
+# 1 TRY FOR JSON
 conn.request("GET", "/listSpecies?json=1")
-result = conn.getresponse()
-print("Response received!: {} {}\n".format(result.status, result.reason))
-data = result.read().decode("utf-8")
-response = json.loads(data)
-print(response)
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
 
-# -- 2 TRY FOR JSON
+# 2 TRY FOR JSON
 conn.request("GET", "/karyotype?specie=mouse&json=1")
-result = conn.getresponse()
-print("Response received!: {} {}\n".format(result.status, result.reason))
-data = result.read().decode("utf-8")
-response = json.loads(data)
-print(response)
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
 
-# 3 -- TRY FOR JSON
+# 3 TRY FOR JSON
 conn.request("GET", "/chromosomeLength?specie=mouse&chromo=18&json=1")
-result = conn.getresponse()
-print("Response received!: {} {}\n".format(result.status, result.reason))
-data = result.read().decode("utf-8")
-response = json.loads(data)
-print(response)
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
 
-# 4 -- TRY FOR JSON
+# 4 TRY FOR JSON
 conn.request("GET", "/geneSeq?gene=FRAT1&json=1")
-result = conn.getresponse()
-print("Response received!: {} {}\n".format(result.status, result.reason))
-data = result.read().decode("utf-8")
-response = json.loads(data)
-print(response)
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
 
-# 5 -- TRY FOR JSON
+# 5 TRY FOR JSON
 conn.request("GET", "/geneInfo?gene=FRAT1&json=1")
-result = conn.getresponse()
-print("Response received!: {} {}\n".format(result.status, result.reason))
-data = result.read().decode("utf-8")
-response = json.loads(data)
-print(response)
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
 
-# 6 -- TRY FOR JSON
+# 6 TRY FOR JSON
 conn.request("GET", "/geneCalc?gene=FRAT1&json=1")
-result = conn.getresponse()
-print("Response received!: {} {}\n".format(result.status, result.reason))
-data = result.read().decode("utf-8")
-response = json.loads(data)
-print(response)
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
 
-# 7 -- TRY FOR JSON
+# 7 TRY FOR JSON
 conn.request("GET", "/geneList?chromo=1&start=0&end=30000&json=1")
-result = conn.getresponse()
-print("Response received!: {} {}\n".format(result.status, result.reason))
-data = result.read().decode("utf-8")
-response = json.loads(data)
-print(response)
+r1 = conn.getresponse()
+print("Response received!: {} {}\n".format(r1.status, r1.reason))
+data1 = r1.read().decode("utf-8")
+response = json.loads(data1)
+print (response)
