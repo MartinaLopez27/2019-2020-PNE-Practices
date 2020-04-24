875a309df5d56759ae189a7a0f7b53d512413231
MartinaLopez27 <m.lopezgo.2019@alumnos.urjc.es>
2020-04-24 13:10:47 +0200

    Session20 -

--- Final-Project/Seq.py
+++ Final-Project/Seq.py
@@ -1,3 +1,4 @@
+"""""
 class Seq:
     def __init__(self, strbase):  # -- Initialize variables
         self.strbase = strbase
@@ -33,3 +34,25 @@ class Seq:
 
     def perc(self, base):  # -- Get the percentage
         return round((float(self.count(base)) / float(self.len())) * 100, 1)
+"""""
+
+class Seq:
+    """A class for representing sequences"""
+    def __init__(self, strbases):
+        self.strbases = strbases
+
+    def len(self):
+        return len(self.strbases)
+
+    def count(self, base):
+        n_base = self.strbases.count(base)
+        return n_base
+
+    def perc(self, base):
+        tl = len(self.strbases)
+        if tl > 0:
+            nbase = self.count(base)
+            perc = round(100.0 * nbase / tl, 1)
+        else:
+            perc = 0
+        return perc
